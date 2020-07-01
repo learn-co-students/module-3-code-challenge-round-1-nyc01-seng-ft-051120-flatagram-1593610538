71be32ae3d76366c925ff411724fe915b7694457
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-07-01 10:26:36 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -3,18 +3,38 @@
 
 //See the image received from the server, including its title, likes and comments when the page loads
     //function getImages
-    //renderImages
+    //renderImage
     // title: "", likes: , comments: ""
 
 // Click on the heart icon to increase image likes, and still see them when I reload the page
     // remember the function nodefault on refresh on this ^^
+    // const heartButton
 
 // Add a comment (no persistance needed)
 
+const images = []
 
 document.addEventListener("DOMContentLoaded",(event)=>{
     console.log('DOM loaded');
 
+    getImages()
+
 
 });
 
+function getImages(){
+    fetch('http://localhost:3000/images/1')
+    .then(response => response.json())
+    .then(images => console.log(images))
+    
+}
+
+function renderImage(){
+    fetch('http://localhost:3000/images/1'),{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }
+}
