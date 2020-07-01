932bb56bec67a4fd917d61310d6186a361b15d70
cathjime <xcatherine.jimenez@gmail.com>
2020-07-01 10:06:23 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 // write your code here
+document.addEventListener("DOMContentLoaded", function(e){
+
+    const imagesContainer = document.getElementsByClassName("image-container")
+
+    function renderImage(imageObject){
+        const imgDiv = document.getElementsByClassName("image-card")
+    }
+
+    function fetchImages(url){
+        fetch(url)
+        .then(resp.json)
+        .then(imagesData => renderImages(imagesData))
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
