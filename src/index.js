d7c7e1a461306224d331f747187a1edebd0f7739
github username <github email address>
2020-07-01 11:26:29 -0400

    5th commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const fetchImage = () => {
   fetch('http://localhost:3000/images/1')
   .then(r => r.json())
   .then(data => {
-    // console.log(data)
+    console.log(data)
     render(data)
   })
 }
@@ -14,9 +14,15 @@ const render = (image) => {
   const photo = document.querySelector('.image')
   const title = document.querySelector('.title')
   const comments = document.querySelector('.comments')
+  const likes = document.querySelector('.likes-section')
+  likes.dataset.likes = image.likes
   comments.innerHTML = ''
   photo.src = image.image
   title.textContent = image.title
+  likes.innerHTML = `
+  <span class="likes">${image.likes} likes</span>
+  <button class="like-button">♥</button>
+  `
 
   const renderComment = (comment) => {
       const li = document.createElement('li')
@@ -31,7 +37,12 @@ const increaseLikes = () => {
   const likesSection = document.querySelector('.likes-section')
   likesSection.addEventListener('click', e => {
     if (e.target.classname = 'like-button') {
-      const likeCounter = document.querySelector()
+      const likes = document.querySelector('.likes-section')
+      const likeCount = parseInt(likes.dataset.likes)
+      const likeObj = {
+        likes: likeCount
+      }
+      console.log(likeObj)
     }
   })
 }
@@ -41,10 +52,11 @@ const postComment = () => {
   const comment = document.querySelector('.comment-form')
   comment.addEventListener('submit', e => {
     e.preventDefault()
+    const input = document.querySelector(".comment-input")
     const commList = document.querySelector('.comments')
     const newComm = document.createElement('li')
-    newComm.textContent = comment.firstChild.value
-    console.log(newComm.textContent)
+    newComm.innerText = input.value
+    commList.append(newComm)
   })
 }
 
