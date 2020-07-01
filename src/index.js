0742e802893c96b7e3d43847bc1c6701e8b8b058
cathjime <xcatherine.jimenez@gmail.com>
2020-07-01 10:26:28 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 // write your code here
 document.addEventListener("DOMContentLoaded", function(e){
 
-    const imagesContainer = document.getElementsByClassName("image-container")
+    const imagesContainer = document.getElementsByClassName("image-container")[0]
+   
 
     function renderImage(imageObject){
-        const imgDiv = document.getElementsByClassName("image-card")
+        const imgDiv = document.getElementsByClassName("image-card")[0]
+        console.log(imgDiv)
+        imgDiv.innerHTML = 
+        `
+        <h2 class="title">${imageObject.title}</h2>
+        <img src="${imageObject.image}" class="image" />
+        `
+        imagesContainer.append(imgDiv)
     }
 
-    function fetchImages(url){
+    function fetchImage(url){
         fetch(url)
-        .then(resp.json)
-        .then(imagesData => renderImages(imagesData))
+        .then(resp => resp.json())
+        .then(imageData => renderImage(imageData))
     }
+    fetchImage("http://localhost:3000/images")
 
 
 
