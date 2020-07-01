7db4f1e0a391ad099e65389b167be042d55398c8
CMSTie <cmstie@gmail.com>
2020-07-01 10:44:28 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(e){
     fetchImage()
+    addComm()
+
+    document.addEventListener('click', function(e){
+        if (e.target.matches('.like-button')){
+            imgId = e.target.dataset.id
+            imgLikes = parseInt(e.target.parentNode.querySelector('.likes').textContent.slice(0,1))
+            newLikes = imgLikes + 1
+            e.target.parentNode.querySelector('.likes').textContent = `${newLikes} likes`
+            fetch('http://localhost:3000/images/1', {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json',
+                    'accepts': 'application/json'
+                },
+                body: JSON.stringify({
+                    'likes': newLikes
+                })
+            })
+        }
+    })
 
 })
 
@@ -21,6 +41,11 @@ function renderImage(image){
     imgCardLike = imgCard.querySelector('.likes')
     imgCardLike.textContent = `${image.likes} likes`
 
+    imgCardLikeBtn = imgCard.querySelector('.like-button')
+    imgCardLikeBtn.dataset.id = image.id
+
+    
+
     imgCardComm = imgCard.querySelector('.comments')
     imgCardComm.textContent = ''
 
@@ -32,4 +57,21 @@ function renderImage(image){
     })
 
     
+}
+
+function addComm(){
+    document.addEventListener('submit', function(e){
+        e.preventDefault()
+        commForm = e.target
+        
+        comment = commForm.querySelector('.comment-input').value
+
+        commLi = document.createElement('li')
+        commLi.textContent = comment
+        
+        commUl = commForm.parentNode.querySelector('.comments')
+        commUl.append(commLi)
+
+        commForm.reset()
+    })
 }
