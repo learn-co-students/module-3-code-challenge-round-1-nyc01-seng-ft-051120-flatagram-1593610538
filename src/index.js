73cdedea44e0057df4cc229e9f4c8c05d44677f2
smatalha <sma.talha@gmail.com>
2020-07-01 10:17:24 -0400

    fetched, loaded title, image,likes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-// write your code here
+document.addEventListener('DOMContentLoaded', () =>{
+    // const baseURL = 'http://localhost:3000'
+    fetch('http://localhost:3000/images/1')
+    .then(response => response.json())
+    .then(image => {
+        renderImage(image)
+    })
+
+    const renderImage = image => {
+        let title = document.querySelector('.title')
+        title.innerText = image.title
+
+        let img = document.querySelector('.image')
+        img.src = image.image
+
+        let likes = document.querySelector('.likes')
+        likes.innerText = `${image.likes} likes`
+
+        let comment = document.querySelector('.comments')
+        comment.innerHTML = image.comment
+        console.log(comment);
+        
+    }
+})
+
+/*
+1. fetch the image
+2. add title ,likes & comments on DOM
+ */
