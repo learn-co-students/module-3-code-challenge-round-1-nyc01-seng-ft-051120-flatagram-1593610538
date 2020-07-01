40494030ee7ad7d23174874ee5ecbde87468326b
github username <github email address>
2020-07-01 10:06:20 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 // write your code here
+
+
+const fetchImage = () => {
+  fetch('http://localhost:3000/images/1')
+  .then(r => r.json())
+  .then(data => {
+    console.log(data)
+    render(data)
+  })
+}
+
+const render = (image) => {
+  const photo = document.querySelector('.image')
+  const title = document.querySelector('.title')
+  const likes = document.querySelector('.likes')
+  photo.src = image.image
+  title.textContent = image.title
+  likes.textContent = `${image.likes} likes`
+}
+
+fetchImage()
