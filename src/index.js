7ec5095f56c7066397e2360ac16bd63774b3ada3
chloekim917 <chloekim917@gmail.com>
2020-07-01 10:06:23 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-// write your code here
+document.addEventListener("DOMContentLoaded", () => {
+    getImage()
+    like()
+})
+
+function getImage(){
+    fetch('http://localhost:3000/images/1')
+    .then(resp => resp.json())
+    .then(image => { render(image) 
+    })
+}
+
+function render(image){
+    const imageCard = document.querySelector('.image-card')
+    imageCard.innerHTML = `
+    <h2 class="title">${image.title}</h2>
+        <img src="${image.image}" class="image" />
+        <div class="likes-section">
+          <span class="likes">${image.likes} likes</span>
+          <button class="like-button">♥</button>
+    `
+}
+
+function like(){
+    const likeBtn = document.querySelector('.like-button')
+    const likes = document.querySelector('.likes')
+    console.log(likes)
+    // let likeNum = parseInt(likes)
+    // console.log(likeNum)
+
+    likeBtn.addEventListener('click', function(e){
+        e.preventDefault()
+        
+        
+    })
+}
