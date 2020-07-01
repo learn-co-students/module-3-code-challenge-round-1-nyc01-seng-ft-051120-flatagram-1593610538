fdce26c6c25ea350dc55712952d258d34549edb7
grantkoji <grantkoji@gmail.com>
2020-07-01 11:13:05 -0400

    with comment database/delete

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const imgContainerDiv = document.querySelector(".image-container")
 
 document.addEventListener('DOMContentLoaded', function(){
     fetchImage(imgIndexUrl, 1)
+    fetchComments(commentsIndexUrl)
 
 
     removeInitialComments()
@@ -22,8 +23,30 @@ document.addEventListener('DOMContentLoaded', function(){
         e.preventDefault()
         const form = e.target
         const comment = form.comment.value
-        addCommentWithoutPersistance(comment)
+        if (comment === "") {
+            alert("Invalid Entry: You must say something")
+        }
+        else {
+            addCommentWithoutPersistance(comment)
+            const imageId = form.dataset.imgid
+            const commentObj = createCommentObj(imageId, comment)
+            addCommentToDatabase(commentsIndexUrl, commentObj) 
+        }
+
         form.reset()
+    })
+
+    const commentUl = document.querySelector('.comments')
+    commentUl.addEventListener('click', function(e){
+        if (e.target.className = 'delete-button') {
+            const deleteButton = e.target
+            const imgId = deleteButton.dataset.imageid
+            const commentId = deleteButton.dataset.id
+            removeComment(commentsIndexUrl, commentId)
+            removeCommentFromLi(commentId)
+        }
+
+
     })
 })
 
@@ -51,6 +74,9 @@ function renderImage(imgData) {
     const likeButton = imgContainerDiv.querySelector('button')
     likeButton.dataset.likes = imgLikes
     likeButton.dataset.id = imgId
+
+    const commentForm = imgContainerDiv.querySelector('.comment-form')
+    commentForm.dataset.imgid = imgId
 }
 
 function fetchPatchLike(url, imageId, likeCount) {
@@ -89,4 +115,83 @@ function removeInitialComments() {
     })
 }
 
+
+
+function addCommentToDatabase(url, commentObj) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(commentObj)
+    })
+    .then(resp => resp.json())
+    .then(commentData => commentData)
+    // .then(error => alert(error))
+}
+
+
+
+function createCommentObj(imageId, comment) {
+    const obj = {
+                    "imageId": `${imageId}`,
+                    "content": `${comment}`
+
+                }
+    return obj
+}
+
+
+
+function fetchComments(url) {
+    fetch(url)
+    .then(resp => resp.json())
+    .then(commentData => renderComments(commentData))
+    .catch(error => alert(error))
+}
+
+function renderComments(commentData) {
+    commentData.forEach(comment => renderComment(comment))
+}
+
+function renderComment(comment) {
+    const imageId = comment.imageId
+    const commentContent = comment.content 
+    const commentId = comment.id
+    const commentLi = document.createElement('li')
+    commentLi.dataset.id = commentId 
+    commentLi.dataset.imageid = imageId
+    commentLi.textContent = commentContent 
+    const deleteButton = document.createElement('button')
+    deleteButton.dataset.id = commentId 
+    deleteButton.dataset.imageid = imageId
+    deleteButton.textContent = 'x'
+    deleteButton.className = 'delete-button'
+    commentLi.append(deleteButton)
+
+    const commentUl = document.querySelector('.comments')
+    commentUl.append(commentLi)
+
+}
+
+function removeComment(url, commentId) {
+    fetch(`${url}/${commentId}`, {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json',      
+        'Accept': 'application/json'      
+    }})
+    // .then(resp => {
+    //     removeCommentFromLi(commentID)
+    // })
+    .catch(error => alert('There was an Error: This Comment Has Not Been Deleted From Our Database'))
+
+}
+
+function removeCommentFromLi(commentId) {
+    const commentUl = document.querySelector('.comments')
+    const commentLi = commentUl.querySelector(`[data-id="${commentId}"]`)
+    commentLi.remove()
+}
 
