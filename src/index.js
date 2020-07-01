afb487951591f0e5d1483df4c5b3cae8e40ea07e
yuksalbek97 <yuksal97@gmail.com>
2020-07-01 10:06:21 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+    // when the page loads, fetch comments
+    fetchComments()
+
+})
+
+
+function fetchComments(){
+    fetch('http://localhost:3000/comments')
+    .then (resp => resp.json())
+    .then(comments_obj => renderComments(comments_obj))
+}
+
+function renderComments(comments_obj){
+    const commmentContainer = document.getElementsByClassName('.comments')
+    
+}
