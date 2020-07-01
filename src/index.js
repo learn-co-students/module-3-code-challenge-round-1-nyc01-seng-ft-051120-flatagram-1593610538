a5916d3e7b67d97788bc983ee1d0cc27339e14c6
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-07-01 10:47:11 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
 // write your code here
 document.addEventListener('DOMContentLoaded', () => {
-const renderImage = data =>{
- const imageFrame = document.getElementsByClassName('images')
-.log(imageFrame)
+const likeHandler = () => {
+  const likeButton = document.getElementsByClassName("like-button")[0]
+
+   likeButton.addEventListener("click", function(e){
+          increade
+   })
 }
+
+const renderImage = data =>{
+ const title = document.getElementsByClassName("title")[0]
+ const imageFrame = document.images[1]
+ const likes =  document.getElementsByClassName("likes")
+ const ul = document.getElementsByClassName("comments")[0]
+ const comments = data.comments
+  title.innerText = data.title
+  ul.innerHTML = ""
+ comments.forEach((comment) => {
+   const ul = document.getElementsByClassName("comments")[0]
+    const li = document.createElement("li")
+    li.innerText = comment.content
+    ul.appendChild(li)
+
+    });
+
+ likes[0].innerText = `${data.likes} likes`
+   console.log( data.likes)
+   imageFrame.src = data.image
+}
+
 const getImage = () => {
   fetch("http://localhost:3000/images/1")
   .then(response => response.json())
        .then(data => {
          console.log('Success:', data);
 
-             // renderImage(data)
+            renderImage(data)
          })
        .catch((error) => {
          console.error('Error:', error);
        })
     }
+
+
+
    getImage()
+   likeHandler()
 //DOMContentLoaded
 })
