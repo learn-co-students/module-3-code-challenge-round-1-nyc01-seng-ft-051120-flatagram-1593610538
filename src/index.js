62e5d4c70c9418313eb021e4e3924d68b9484798
David <davidspector08@gmail.com>
2020-07-01 10:14:38 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-// write your code here
+
+
+const imageCard = document.querySelector(`.image-container`)
+const likesSpan = document.querySelector(`.likes`)
+const commentsUL = document.querySelector(`.comments`)
+const titleH1 = document.querySelector(`.title`)
+const likeButton = document.querySelector(`.like-button`)
+const commentForm = document.querySelector(`.comment-form`)
+const commentButton = document.querySelector(`.comment-button`)
+
+
+imageUrl =  `http://localhost:3000/images/1`
+commentsUrl =  `http://localhost:3000/comments`
+commentUrl = `http://localhost:3000/comments/:id`
+
+
+// - See the image received from the server, including its title, likes and comments when the page loads
+
+
+
+// dom content loaded with render image function
+//getImage function
+//fetch to imageUrl 
+//renderimage function called
+
+// 1.
+//renderImage function (image)
+// take image url title likes and comments
+//set each as info from image 
+// set likes and id in dataset
+
+//2. click event listener on like button
+// inside event listener a function addLikes
+// else if target is form, submit listener
+
+//add likes:
+// takes currentLikes and adds a like
+//current likes comes from dataset on likes 
+// patch request updates likes
+// note: you can increase likes without reloading the page, but it makes a post each click
+
+// 3. addComment
+// define form as form
+// create commentObj with comment text
+// post request with comment obj
+// renderpage function
+
+
+
+
+
+
+
+// - Click on the heart icon to increase image likes, and still see them when I reload the page
+// - Add a comment (no persistance needed)
