c80f16fa7d2483e7a441e66ca4fcbc0c13e1b6ce
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-07-01 10:26:52 -0400

    fetching comments

--- src/index.js
+++ src/index.js
@@ -10,15 +10,25 @@ fetch("http://localhost:3000/images")
 .then(images => renderImage(images))
 }
 
-function renderImage(img){
-    let image = document.querySelector("body > div > div > img")
-    console.log(image);
-    
-    let picture = img.image
-    console.log(picture);
-    
+function fetchComments(){
+fetch("http://localhost:3000/comments")
+.then(resp => resp.json())
+.then(comments => renderComments(comments))
+}
 
-    // image.append(picture)
+function renderImage(img){
+    let imagePlace = document.querySelector("body > div > div > img")
+    let titlePlace = document.querySelector("body > div > div > h2")
+    let picture = img[0].image
+    let title = img[0].title
+    imagePlace.src = picture
+    titlePlace.innerText = title
+    console.dir(imagePlace.src);
+}
+
+function renderComments(com){
+
+    
 }
 
 fetchImage()
