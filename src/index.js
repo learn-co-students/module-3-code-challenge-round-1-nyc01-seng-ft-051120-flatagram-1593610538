a62dd8fce2cbcf760cf32aa23316c7a68a1ac94e
github username <github email address>
2020-07-01 10:47:08 -0400

    third commit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,40 @@
 // write your code here
 // - See the image received from the server, including its title, likes and comments when the page loads
 document.addEventListener('DOMContentLoaded', () => {
+ const BASE_IMG = `http://localhost:3000/images`
+ const BASE_COMM = `http://localhost:3000/comments`
+ fetchImages()
+ fetchComments()
 
- const BaseImage = `http://localhost:3000/images`
- const BaseComments = `http://localhost:3000/comments`
 
 
 
     function fetchImages(){
-        fetch(`http://localhost:3000/images`)
+        fetch(BASE_IMG)
         .then(resp => resp.json())
-        .then(console.log)
+        .then(images => getImages(images))
 
-    }
-    // }  function fetchComments(){
-    //     fetch(BaseComments)
+    }  
+    
+    // function fetchComments(){
+    //     fetch(BASE_COMM)
     //     .then(resp => resp.json())
-    //     .then(comments => getImages(comments))
+    //     .then(comments => getComments(comments))
     // }
    
-    // function getImages(images, comments) {
+    function getImages(images) {
+        console.log(images)
+        const imageCard = document.querySelector(".image-card")
+        images.forEach(image => {
+            let picture = document.querySelector(".image")
+            picture.innerText = ${image.image}
+            let title = document.querySelector(".title")
+            title.innerText = ${image.title}
+            let likes = document.querySelector(".likes")
+            likes.textContent = ${image.likes}
+        })
         
-        // const newImage = document.querySelector(".image-card")
-        // images.forEach(image => {
-        //     document.querySelector(".image").innerText = ${image.image}
-        //     document.querySelector(".title").innerText = ${image.title}
-        //     document.querySelector(".likes").innerText = ${image.likes}
-    //     })
-    //     comments.forEach(comment => {
-    //         document.querySelector("")
-    //     })
-    // }
+    }
 
 
 })
