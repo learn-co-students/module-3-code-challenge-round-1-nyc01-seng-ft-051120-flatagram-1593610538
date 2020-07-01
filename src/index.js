8f5b35b0abad829d3e882efcae36da48cb42229b
github username <github email address>
2020-07-01 10:26:38 -0400

    2nd commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const fetchImage = () => {
   fetch('http://localhost:3000/images/1')
   .then(r => r.json())
   .then(data => {
-    console.log(data)
+    // console.log(data)
     render(data)
   })
 }
@@ -14,9 +14,35 @@ const render = (image) => {
   const photo = document.querySelector('.image')
   const title = document.querySelector('.title')
   const likes = document.querySelector('.likes')
+  const comments = document.querySelector('.comments')
+  comments.innerHTML = ''
   photo.src = image.image
   title.textContent = image.title
   likes.textContent = `${image.likes} likes`
+
+  const renderComment = (comment) => {
+      const li = document.createElement('li')
+      li.textContent = comment.content 
+      comments.appendChild(li)
+  }
+
+  image.comments.forEach(comment => renderComment(comment))
+}
+
+const increaseLikes = () => {
+  const likesSection = document.querySelector('.likes-section')
+  likesSection.addEventListener ('click', e => {
+    if (e.target.className === 'like-button') {
+      const likes = document.querySelector('.likes')
+      console.log(likes)
+
+      // fetch('http://localhost:3000/images/1', {
+      //   method: 'PATCH',
+
+      // })
+    }
+  })
 }
 
 fetchImage()
+increaseLikes()
