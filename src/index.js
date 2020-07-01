439eaf07d99486b5f8848295e7157a9bd0dd9df6
github username <github email address>
2020-07-01 10:15:29 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // write your code here
+document.addEventListener('DOMContentLoaded',function(){  
+let getPictureCard = document.querySelector('.image-card')
+
+const picApi = () => {fetch('http://localhost:3000/images?_embed=comments')
+.then(resp => resp.json())
+.then(json => json.forEach(pic => showPic(pic)))}
+
+const showPic = (pic) =>{
+  getPictureCard.dataset.id = pic.id
+  let title = getPictureCard.querySelector('.title')
+  let image = getPictureCard.querySelector('.image')
+  let like = getPictureCard.querySelector('.likes')
+  title.textContent = pic.title
+  image.src= pic.image
+  like.textContent = `${pic.likes} likes`
+
+}
+
+picApi()
+})
