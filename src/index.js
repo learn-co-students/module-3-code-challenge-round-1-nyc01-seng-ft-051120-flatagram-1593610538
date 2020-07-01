4a0b05321afdbeb67d7adb25ed989363f19323ef
steveneross94 <ross.steven@me.com>
2020-07-01 10:26:30 -0400

    core deliverables done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // write your code here
 // √See the image received from the server, including its title, likes and comments when the page loads
 // √Click on the heart icon to increase image likes, and still see them when I reload the page
-// Add a comment (no persistance needed)
+// √Add a comment (no persistance needed)
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -9,6 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const imageCard = document.querySelector(".image-card")
     const likeSpan = document.querySelector(".likes")
     const likeBtn = document.querySelector(".like-button")
+    const commentsUl = document.querySelector(".comments")
+    console.log(commentsUl);
+    
 
     
 // need an event listener for clicks
@@ -19,6 +22,27 @@ document.addEventListener("click", e => {
         patchLikes(e)
     }
 })
+
+// need event listener for submit
+    // submit should be prevented from reloading page
+    // need to find comment value and append to comments <ul>
+document.addEventListener("submit", e => {
+    e.preventDefault()
+    if (e.target.className === "comment-form") {
+        addComment(e)
+    }
+})
+
+const addComment = (e) => {
+    let form = e.target
+    console.log(form);
+    let newComment = form.comment.value
+    console.log(newComment);
+    let commentLi = document.createElement("li")
+    commentLi.innerText = `${newComment}`
+    commentsUl.append(commentLi)
+}
+
 
  const patchLikes = (e) => {
      let likeId = e.target.dataset.id
@@ -33,29 +57,27 @@ document.addEventListener("click", e => {
      })
      .then(r => r.json())
      .then(likes => {
-        //  likeSpan.innerText = likes
+        likeSpan.innerText = `${likes.likes} likes`     
      })
-     
  }
 
-
-
 const renderImages = (images) => {
-    images.forEach(image => {
-        imageCard.children[0].innerText = `${image.title}` 
-        imageCard.children[1].src = `${image.image}`
-        
-        likeSpan.innerText = `${image.likes} likes`
-        likeBtn.dataset.id = `${image.id}`
-    })
+        imageCard.children[0].innerText = `${images.title}` 
+        imageCard.children[1].src = `${images.image}`
+        likeSpan.innerText = `${images.likes} likes`
+        likeBtn.dataset.id = `${images.id}`
+        commentsUl.innerHTML = `
+        <li>${images.comments[0].content}</li>
+        <li>${images.comments[1].content}</li>
+        <li>${images.comments[2].content}</li>
+        `
 }    
 
 const fetchImage = () => {
-    fetch(baseUrl+'/images')
+    fetch(baseUrl+'/images/1')
     .then(r => r.json())
     .then(images => renderImages(images))
 }
-
-    
+  
 fetchImage()
 })
