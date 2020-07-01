90a5b0d6b9ec93ebf3ad10c746d10460fbc38ac9
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-07-01 10:06:35 -0400

    problem with fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+    
+    let commentsPlace = document.querySelector("body > div > div > ul")
+// 1. See the image received from the server, including its title, likes and comments when the page loads
+
+function fetchImage(){
+fetch("http://localhost:3000/images")
+.then(resp => resp.json())
+.then(images => renderImage(images))
+}
+
+function renderImage(img){
+    let image = document.querySelector("body > div > div > img")
+    console.log(image);
+    
+    let picture = img.image
+    console.log(picture);
+    
+
+    // image.append(picture)
+}
+
+fetchImage()
+})
+
+
+
+
+
+// 2. Click on the heart icon to increase image likes, and still see them when I reload the page
+
+
+
+
+// 3.  Add a comment (no persistance needed)
