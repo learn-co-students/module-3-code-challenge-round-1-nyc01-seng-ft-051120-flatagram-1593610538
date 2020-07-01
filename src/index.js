654d8f22ca866069721f9271b812a67ee160139a
JeanmarieJackman <jeanmarie.jackman@gmail.com>
2020-07-01 10:06:58 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // write your code here
+//As a user, I can:
+
+//See the image received from the server, including its title, likes and comments when the page loads
+    //function getImages
+    //renderImages
+    // title: "", likes: , comments: ""
+
+// Click on the heart icon to increase image likes, and still see them when I reload the page
+    // remember the function nodefault on refresh on this ^^
+
+// Add a comment (no persistance needed)
+
+
+document.addEventListener("DOMContentLoaded",(event)=>{
+    console.log('DOM loaded');
+
+
+});
+
