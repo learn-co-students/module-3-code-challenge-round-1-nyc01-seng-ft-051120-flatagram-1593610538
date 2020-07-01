855c552a116f73c5ee3455e1f1d4ee2c8ea3cafc
ra-bluebirdgroup <ra@bluebirdgroup.ltd>
2020-07-01 10:06:36 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // write your code here
+document.addEventListener('DOMContentLoaded', () => {
+const renderImage = data =>{
+ const imageFrame = document.getElementsByClassName('images')
+.log(imageFrame)
+}
+const getImage = () => {
+  fetch("http://localhost:3000/images/1")
+  .then(response => response.json())
+       .then(data => {
+         console.log('Success:', data);
+
+             // renderImage(data)
+         })
+       .catch((error) => {
+         console.error('Error:', error);
+       })
+    }
+   getImage()
+//DOMContentLoaded
+})
